7be9576c40c031bf77fd0687a6d8f878b67391d5
Simon Gowing <simongowing1@gmail.com>
2021-02-24 20:40:43 +0100

    final commit Wednesday

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import './App.css';
+import users from "./users";
 
 class App extends React.Component {
 
+state = {
+  ironhackersList: users,
+  search: ''
+}
 
   render() {
-    (
-      < div ></div >
+    
+const displayIronhackers = this.state.ironhackersList.map((ironhacker) => {
+  return(
+  <tr>
+    <td>{ironhacker.firstName}</td>
+    <td>{ironhacker.lastName}</td>
+    <td>{ironhacker.campus}</td>
+    <td>{ironhacker.role}</td>
+  </tr>
+  )
+})
+
+    return (
+      <div>
+      <form>
+      <label htmlFor
+      </form>
+      
+      <table>
+        <tr>
+        <th>First name</th>
+        <th>Last name</th>
+        <th>Campus</th>
+        <th>Role</th>
+        </tr>
+      {displayIronhackers}
+      </table>  
+      </div>
     );
   }
 }
